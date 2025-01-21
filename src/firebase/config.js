a91e4598381588397931d07437e915a1f49b9fe8
soyadem <fraadem@outlook.dk>
2025-01-21 10:29:45 +0100

    project creation

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcTtyDL7TzbGHsb_kjc9kHJCSXk_m4bM8",
+    authDomain: "youthconnect-2025.firebaseapp.com",
+    projectId: "youthconnect-2025",
+    storageBucket: "youthconnect-2025.firebasestorage.app",
+    messagingSenderId: "853669693698",
+    appId: "1:853669693698:web:38ac3018567fbe004fda1b"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init services
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+  
+  //timestamp
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+  export { projectFirestore, projectAuth, timestamp }
