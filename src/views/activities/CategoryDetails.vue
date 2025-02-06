87b258566b3bdcbf96399ef219312e2ff7ce4dca
soyadem <fraadem@outlook.dk>
2025-02-07 00:23:37 +0100

    upd

--- src/views/activities/CategoryDetails.vue
+++ src/views/activities/CategoryDetails.vue
@@ -1,28 +1,27 @@
 <template>
   <div v-if="error">{{ error }}</div>
   <div v-if="document">
-    <h2>Category Details - ID: {{ id }}</h2>
+    <h2>CATEGORY DETAILS - ID: {{ id }}</h2>
     <div class="single">
       <div class="thumbnail">
         <img :src="document.coverUrl" alt="Category image" />
       </div>
       <div class="info">
         <h3>{{ document.title }}</h3>
-        <p>Created by {{ document.userName }}</p>
+        <p>CREATED BY: {{ document.userName }}</p>
       </div>
       <div class="song-number">
-        <p>{{ document.activities.length }} Activities</p>
+        <p>{{ document.activities.length }} ACTIVITIES</p>
       </div>
     </div>
   </div>
   <div v-else>
-    <p>Loading...</p>
+    <p>LOADING...</p>
   </div>
 </template>
 
 <script>
 import getDocument from '../../composables/getDocument'
-import { computed } from 'vue'
 
 export default {
   props: ['id'],
@@ -34,5 +33,42 @@ export default {
 </script>
 
 <style>
-/* Add your styles */
+  .playlist-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 80px;
+  }
+  .cover {
+    overflow: hidden;
+    border-radius: 20px;
+    position: relative;
+    padding: 160px;
+  }
+  .cover img {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 200%;
+    max-height: 200%;
+  }
+  .playlist-info {
+    text-align: center;
+  }
+  .playlist-info h2 {
+    text-transform: capitalize;
+    font-size: 28px;
+    margin-top: 20px;
+  }
+  .playlist-info p {
+    margin-bottom: 20px;
+  }
+  .username {
+    color: #999;
+  }
+  .description {
+    text-align: left;
+  }
 </style>
